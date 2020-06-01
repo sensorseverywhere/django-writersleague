eb92802d6dec05034475c77a8e2e52df3c913bbb
wade <wade_mansell@hotmail.com>
2020-06-01 21:10:18 +1000

    added admin

--- server/account/admin.py
+++ server/account/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Address, Profile, CustomUser
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('address1', 'address2', 'city', 'post_code', 'country')
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'active')
+
+
+@admin.register(CustomUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'user_type')
