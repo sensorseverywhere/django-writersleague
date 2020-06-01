c2c4a4614a61cc2dcc8af7f560b64087caa3ffd5
wade <wade_mansell@hotmail.com>
2020-06-01 13:20:16 +1000

    back to square

--- server/stories/urls.py
+++ server/stories/urls.py
@@ -8,3 +8,4 @@ urlpatterns = [
     path('', StoryListView.as_view(), name="story_list"),
     path('story/create/', StoryCreateView.as_view(), name="story_create"),
     path('story/<int:pk>/', StoryDetailView.as_view(), name="story_detail"),
+]
