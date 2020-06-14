993a25d88809ba6141429b964697d1b2cf07357f
wade <wade_mansell@hotmail.com>
2020-06-15 09:28:11 +1000

    material design and static images  / css

--- server/writersleague/urls.py
+++ server/writersleague/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('plotify/', admin.site.urls),
     path('', include('account.urls')),
     path('', include('cart.urls')),
     path('', include('orders.urls')),
