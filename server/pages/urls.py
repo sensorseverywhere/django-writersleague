dee698e3837319b65c45a34fca177f489a922c6a
wade <wade_mansell@hotmail.com>
2020-06-16 11:59:57 +1000

    server/static/images was missing

--- server/pages/urls.py
+++ server/pages/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from .views import HomePageView, AboutPageView, ContactPageView, ThanksPageView
 
+
+app_name = "pages"
+
 urlpatterns = [
     path('', HomePageView.as_view(), name="home"), 
     path('about/', AboutPageView.as_view(), name="about"), 
