a70b63eaadef33f9a424637fb7769cba0e3a67e4
wade <wade_mansell@hotmail.com>
2020-06-22 02:18:03 +1000

    First passing test! <-- using pytest

--- server/stories/tests/test_models.py
+++ server/stories/tests/test_models.py
@@ -1,16 +1,27 @@
-# import pytest
+import json
 
-# from stories.models import Story
+import pytest
+from account.models import CustomUser
+from stories.models import Story
 
 
-# @pytest.mark.django_db
-# def test_story_model():
-#     story = Story(
-#         title="World's Greatest...",
-#         content="Story Content",
-#         status=0,
-#         genre=1,
-#         votes=34,
-#         live=True
-#     )
-#     story.save()
+@pytest.mark.django_db
+def test_story_model(client):
+    user = CustomUser.objects.create_user(
+        "test@test.com",
+    )
+
+    story = Story(
+        title="World's Greatest...",
+        content="Story Content",
+        author=user,
+        status=0,
+        genre=1,
+        votes=34,
+        live=True
+    )
+    story.save()
+    assert story.title == "World's Greatest..."
+    assert story.content == "Story Content"
+    assert story.votes == 34
+    assert str(story) == story.title
