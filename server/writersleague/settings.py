dacdd1dfbf2864e5aecfe16faa9cd56d6f1ecec5
wade <wade_mansell@hotmail.com>
2020-08-03 16:29:42 +1000

    update mg sender domain

--- server/writersleague/settings.py
+++ server/writersleague/settings.py
@@ -174,7 +174,7 @@ MAILGUN_SERVER_NAME = "smtp.mailgun.org"
 
 ANYMAIL = {
     "MAILGUN_API_KEY": os.environ.get('MAILGUN_API_KEY'),
-    "MAILGUN_SENDER_DOMAIN": 'mg.thewritersleague.net',
+    "MAILGUN_SENDER_DOMAIN": 'mg.www.thewritersleague.net',
 }
 
 APPEND_SLASH = False
