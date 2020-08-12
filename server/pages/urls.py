8e060a068a996a1175991fb39c6ea617d6ca0a36
wade <wade_mansell@hotmail.com>
2020-08-13 07:32:59 +1000

    under construction

--- server/pages/urls.py
+++ server/pages/urls.py
@@ -5,29 +5,29 @@ from . import views
 app_name = "pages"
 
 urlpatterns = [
-    # path(
-    #     '',
-    #     views.ComingSoonPageView.as_view(),
-    #     name="coming-soon"
-    #     ),
     path(
         '',
+        views.ComingSoonPageView.as_view(),
+        name="coming-soon"
+        ),
+    path(
+        'temphomepage/',
         views.HomePageView.as_view(),
         name="home"
         ),
-    path(
-        'about/',
-        views.AboutPageView.as_view(),
-        name="about"
-        ),
-    path(
-        'contact/',
-        views.ContactFormView.as_view(),
-        name="contact"
-        ),
-    path(
-        'thanks/',
-        views.ThanksPageView.as_view(),
-        name="thanks"
-        ),
+    # path(
+    #     'about/',
+    #     views.AboutPageView.as_view(),
+    #     name="about"
+    #     ),
+    # path(
+    #     'contact/',
+    #     views.ContactFormView.as_view(),
+    #     name="contact"
+    #     ),
+    # path(
+    #     'thanks/',
+    #     views.ThanksPageView.as_view(),
+    #     name="thanks"
+    #     ),
 ]
