41a67969226d80222ed13e978b3607c93ffe59aa
wade <wade_mansell@hotmail.com>
2020-08-18 08:47:17 +1000

    added comp button to menu

--- server/static/js/editor.js
+++ server/static/js/editor.js
@@ -1,7 +1,8 @@
 class Editor {
     constructor(count) {
         this.count = count;
-        this.compare = [];
+        this.comp();
+        this.isCompetition = false;
         document.addEventListener('DOMContentLoaded', () => {
             this.logKeys();
         });
@@ -13,7 +14,6 @@ class Editor {
             let keyCode = e.hasOwnProperty('which') ? e.which : e.keyCode;
             // working on regexp
             let regexp = new RegExp("(?:^|\\s)").test();
-
             if(keyCode === 32) {
                 // add number of spaces to a count
                 this.count++;
@@ -27,6 +27,19 @@ class Editor {
 
     disableElement() {
         document.querySelector('.ace_text-input').setAttribute('disabled', 'disabled');
+    }
+
+    comp() {
+        let el = document.querySelector('.martor-toolbar');
+        let menu = el.childNodes[0].nextSibling;
+
+        let compButton = document.createElement("button");
+        compButton.appendChild(document.createTextNode("Comp"));
+
+        menu.parentNode.insertBefore(compButton, menu)
+
+   
+
     }
 
 
