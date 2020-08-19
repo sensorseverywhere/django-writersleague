2ccc63906c112aa4de6dadb91c7b725140eb13e9
wade <wade_mansell@hotmail.com>
2020-08-19 12:15:32 +1000

    word count and timer working ( basic, but works ). Show hide comp topbar working; again basic but implemented

--- server/static/js/editor.js
+++ server/static/js/editor.js
@@ -1,11 +1,11 @@
-class Editor {
-    constructor(count) {
-        this.count = count;
-        this.comp();
+class CompetitionEditor {
+    constructor(wordCount) {
+        this.wordCount = wordCount;
         this.isCompetition = false;
         document.addEventListener('DOMContentLoaded', () => {
             this.logKeys();
         });
+        this.comp();
     }
 
     logKeys() {
@@ -16,8 +16,8 @@ class Editor {
             let regexp = new RegExp("(?:^|\\s)").test();
             if(keyCode === 32) {
                 // add number of spaces to a count
-                this.count++;
-                if(this.count >= 5) {
+                this.wordCount++;
+                if(this.wordCount >= 10 && this.wordCount <= 50) {
                     // if the number of spaces >= 500, disable the editor
                     this.disableElement();
                 }
@@ -30,22 +30,79 @@ class Editor {
     }
 
     comp() {
+
         let el = document.querySelector('.martor-toolbar');
         let menu = el.childNodes[0].nextSibling;
 
-        let compButton = document.createElement("button");
+        let compButton = document.createElement("div");
+        compButton.classList.add("btn");
+        compButton.classList.add("btn-comp");
         compButton.appendChild(document.createTextNode("Comp"));
 
-        menu.parentNode.insertBefore(compButton, menu)
+        menu.parentNode.insertBefore(compButton, menu);
 
-   
+        compButton.addEventListener("click", (e) => {
+            e.stopPropagation();
+            let timer = new CountDownTimer(3602);
+            if(this.isCompetition == false) {
+                this.isCompetition = true;
+                if(this.isCompetition == true){
+                    console.log('showing')
+                    document.getElementById('competition-topbar').style.display = "block";
+                    document.getElementById('word-count').innerHTML = this.wordCount;
+                }
+                
+            } else {
+                this.isCompetition = false;
+                if(this.isCompetition == false){
+                    console.log('hidden')
+                    document.getElementById('competition-topbar').style.display = "none";
+                    document.getElementById('word-count').innerHTML = this.wordCount;
+                }
+            }
+            
+        });
+
+        
+ 
 
     }
-
-
 }
 
-let editor = new Editor(0);
+
+
+class CountDownTimer {
+
+    constructor(startTime) {
+        this.startTime, this.timeRemaining = startTime
+        this.seconds = 0;
+        this.minutes = 0;
+        this.hours = 0;
+        this.days = 0;
+        this.initialiseClock(startTime);
+    }
+
+    initialiseClock(startTime) {
+        const timer = setInterval(() => {
+            // timeRemaining is the seconds counter
+            this.timeRemaining -= 1;
+            // log hours, minutes, seconds 
+            this.seconds = this.timeRemaining % 60;
+            this.minutes = Math.floor(this.timeRemaining / 60);
+            this.hours = Math.floor(this.timeRemaining / 3600);
+            this.days = Math.floor((this.timeRemaining / 3600) / 24);
+            // console.log(Math.floor(this.timeRemaining / 3600), Math.floor(this.timeRemaining / 60), this.timeRemaining % 60)
+
+            if(this.timeRemaining == 0) {
+                clearInterval(timer);
+                console.log('Boom!')
+            }
+        }, 1000)
+    } 
+}
+
+
+let editor = new CompetitionEditor(0);
 
 
 
